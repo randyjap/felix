86418c6cbe6a1ba1cb53070ba57fd4e330f499f4
Randy Jap <randyjap@gmail.com>
2019-11-02 10:02:16 +0800

    my first file

---
+++ felix.rb
@@ -0,0 +1,15 @@
+
+
+def run_saving
+  wallet = 0
+
+  while wallet < 10
+    puts "you have #{wallet} dollars"
+    puts "you are saving money"
+    wallet += 1
+  end
+
+  puts "Yay, you have saved 10 dollars"
+end
+
+run_saving
